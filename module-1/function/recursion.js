257ab84e07069b2e94b8abf445ab17573ec2c19b
Tope Taiwo <topzyray@yahoo.com>
2024-11-10 22:05:17 +0100

    Add solution to recursion

--- module-1/function/recursion.js
+++ module-1/function/recursion.js
@@ -1,3 +1,11 @@
-function isEven() {
-    
+function isEven(N) {
+  if (N === 0) {
+    return true;
+  } else if (N === 1) {
+    return false;
+  } else if (N < 0) {
+    return isEven(-N);
+  } else {
+    return isEven(N - 2);
+  }
 }
