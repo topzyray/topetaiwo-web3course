9f6552724b79dcd7490f13f26eadd3edf82880d1
Tope Taiwo <topzyray@yahoo.com>
2024-11-10 21:44:23 +0100

    Add solution to beanCounting

--- module-1/function/beanCounting.js
+++ module-1/function/beanCounting.js
@@ -1,3 +1,19 @@
-function countBs() {}
+function countBs(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "B") {
+      count++;
+    }
+  }
+  return count;
+}
 
-function countChar() {}
+function countChar(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == char) {
+      count++;
+    }
+  }
+  return count;
+}
